7094c361948aa023f613ea08d7eb37c3ee0d701f
Vasiliy Zavoyko <vasiliy.zavoyko@macys.com>
2023-09-27 17:28:13 -0500

    more strict example

--- src/test/java/ru/zavoyko/framework/di/source/data/impl/StarterImpl.java
+++ src/test/java/ru/zavoyko/framework/di/source/data/impl/StarterImpl.java
@@ -6,15 +6,9 @@ import ru.zavoyko.framework.di.source.data.Starter;
 @Slf4j
 public class StarterImpl implements Starter {
 
-    private final WriterImpl logger;
-
-    public StarterImpl(WriterImpl loggerToSet) {
-        this.logger = loggerToSet;
-    }
-
     @Override
     public void start() {
-        logger.log("Started...");
+        log.info("Started...");
     }
 
 }
